f3072263c7e50e515f6e9f39f8bd7757599516f2
Omkar Deshmukh <omkarvd7066@gmail.com>
2021-10-20 21:00:52 +0530

    fix: redux-bugs

---
+++ src/redux/store.js
@@ -0,0 +1,28 @@
+import { createStore } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+    key: "root",
+    storage,
+};
+
+const initialState = {
+    product: [],
+};
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case "add":
+            console.log("actions", action);
+            return { ...state, product: [...action.value] };
+        default:
+            return state;
+    }
+};
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+const store = createStore(persistedReducer);
+const persistor = persistStore(store);
+
+export { store, persistor };
